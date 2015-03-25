27cdfdeb7b401ccea47e352b721b4b42c61692cf
pguo2 <pguo2@demo.com>
2015-03-25 09:52:17 +0800

    again

--- HibernateDemo/src/main/java/com/pguo/hibernate/App.java
+++ HibernateDemo/src/main/java/com/pguo/hibernate/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World 1112!" );
+        System.out.println( "Hello World 11123!" );
     }
 }
